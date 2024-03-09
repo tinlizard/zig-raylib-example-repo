37507a50a76c9b0ab9f3c00868ef49269b9493bb
tinlizard <38041074+tinlizard@users.noreply.github.com>
2024-03-09 15:00:00 +0100

    Create main.zig

---
+++ src/main.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+const raylib = @cImport({
+    @cInclude("raylib.h");
+});
+
+pub fn main() !void {
+    raylib.InitWindow(800, 450, "raylib [core] example - basic window");
+
+    raylib.SetTargetFPS(60);
+
+    while (!raylib.WindowShouldClose()) {
+        raylib.BeginDrawing();
+
+        raylib.ClearBackground(raylib.RAYWHITE);
+
+        // raygui button drawing
+        if (raylib.GuiButton(raylib.Rectangle{ .height = 350, .width = 200, .x = 100, .y = 40 }, "PRESS ME") > 0) {
+            try std.io.getStdOut().writeAll("Button pressed!\n");
+        }
+        raylib.EndDrawing();
+    }
+
+    raylib.CloseWindow();
+}
